341a9a0f99e81ca7eb769d0f2f20cd1016496321
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-01-23 11:22:39 +0100

    Jour 2.3

--- addressbook-angular/src/app/app-routing.module.ts
+++ addressbook-angular/src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './contacts/contact/contact.component';
 import { ContactAddComponent } from './contacts/contact-add/contact-add.component';
+import { ContactShowComponent } from './contacts/contact-show/contact-show.component';
 
 const routes: Routes = [{
   path: 'contacts',
   component: ContactComponent,
-}, {
-  path: 'contacts-add',
-  component: ContactAddComponent,
+  children: [{
+    path: 'add',
+    component: ContactAddComponent,
+  }, {
+    path: ':id',
+    component: ContactShowComponent,
+  }]
 }];
 
 @NgModule({
